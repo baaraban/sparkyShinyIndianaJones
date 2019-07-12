e582f20fc1e65ff7442030331cd4dc0a80a2ad18
Yarema Sirskyy <ysirskyy@lohika.com>
2019-07-12 16:53:31 +0300

    added utils

---
+++ utils/utils.py
@@ -0,0 +1,13 @@
+import requests
+
+def getLatLonFromAddress(address = ""):
+    api_url = 'https://maps.googleapis.com/maps/api/geocode/json'
+    API_key = 'AIzaSyCEE6oOLQ79t5IsEn9SeCE4mirqlFumcOY'
+    params = '?address={}&key={}'.format(address, API_key)
+    response = requests.get(api_url + params)
+    resp_json_payload = response.json()
+    lat_lon = resp_json_payload['results'][0]['geometry']['location']
+    print(resp_json_payload['results'][0]['geometry']['location'])
+    return resp_json_payload['results'][0]['geometry']['location']
+
+#getLatLonFromAddress("Lviv")
