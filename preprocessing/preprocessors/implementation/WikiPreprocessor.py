ee8dd4c0b9056af6f0df7a945be655ad924322fa
baaraban <perepichka@ucu.edu.ua>
2019-07-19 07:57:38 +0300

    Add WikiPreprocessor; Preprocessing takes eternity

---
+++ preprocessing/preprocessors/implementation/WikiPreprocessor.py
@@ -0,0 +1,60 @@
+import re
+
+from pyspark.sql.functions import *
+from pyspark.sql.types import StringType
+from pyspark.sql.functions import pandas_udf, PandasUDFType
+from preprocessing.preprocessors.abstraction.BasePreprocessor import BasePreprocessor
+from utils.text_parser import TextParser
+from datetime import datetime
+import pandas as pd
+
+
+class WikiPreprocessor(BasePreprocessor):
+    _parser = None
+
+    @staticmethod
+    def get_parser():
+        if not WikiPreprocessor._parser:
+            WikiPreprocessor._parser = TextParser()
+        return WikiPreprocessor._parser
+
+    def get_data_source_name(self):
+        return "Wiki data"
+
+    def get_matching_regex(self):
+        return re.compile(r'.*wikidata.*\.csv')
+
+    def preprocess(self, dataframe):
+        current_date = str(datetime.now())
+        new_df = dataframe.withColumn("movement_dates", get_movement_dates("text"))
+        new_df = new_df.withColumn("creation_date",
+                                when(col('inception').isNull() | col('country').isNull(), "{}")\
+                                .otherwise("{" + col('inception') + ":" + col('country') + "}"))
+        new_df = new_df.withColumn("acquiring_date",
+                                   when(col('location').isNull(), "{}") \
+                                   .otherwise("{" + current_date + ":" + col('location') + "}"))
+        new_df = new_df.withColumnRenamed("image", "image_link")
+        new_df = new_df.withColumnRenamed("title", "artifact_name")
+        new_df = new_df.withColumn("transitions", concat('acquiring_date', 'creation_date', 'movement_dates'))
+        return new_df[['artifact_name', 'transitions', 'image_link']]
+
+
+@pandas_udf(returnType=StringType(), functionType=PandasUDFType.SCALAR)
+def get_movement_dates(documents):
+    parser = WikiPreprocessor.get_parser()
+
+    def prepare_element(x):
+        if not x:
+            return None
+        return x.split('Bibliography')[0].strip().replace('\n', '. ').replace('\t', '')
+
+    def get_element(x):
+        if not x:
+            return '{}'
+        transitions = parser.get_text_date_location_per_sentence(x)
+        if transitions:
+            return str(transitions)
+        else:
+            return '{}'
+
+    return pd.Series([get_element(prepare_element(x)) for x in documents])
