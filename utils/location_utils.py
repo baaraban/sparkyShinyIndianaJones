6a7bc1e8a3132ff6bc9278a860a5e31ff285bae5
Yarema Sirskyy <ysirskyy@lohika.com>
2019-07-20 22:36:48 +0300

    added cashing

--- utils/location_utils.py
+++ utils/location_utils.py
@@ -1,7 +1,25 @@
 import requests
-from config import GOOGLE_MAPS_API_KEY, GOOGLE_MAPS_API_URL
+import pickle
+from config import GOOGLE_MAPS_API_KEY, GOOGLE_MAPS_API_URL, LOCATION_CASH_FILE
+
+def getCashedAddress(address):
+    infile = open(LOCATION_CASH_FILE, 'rb')
+    cashed_data = pickle.load(infile)
+    lat_lon_cashed = cashed_data[address]
+    return lat_lon_cashed
+
+def cashAddress(addresses):
+    outfile = open(LOCATION_CASH_FILE, 'wb')
+    pickle.dump(addresses, outfile)
+    outfile.close()
+    return
+
 
 def get_lat_lon(address):
+    lat_lon_cashed = getCashedAddress(address)
+    if lat_lon_cashed:
+        return lat_lon_cashed
+
     params = '?address={}&key={}'.format(address, GOOGLE_MAPS_API_KEY)
     response = requests.get(GOOGLE_MAPS_API_URL + params)
     resp_json_payload = response.json()
@@ -9,8 +27,10 @@ def get_lat_lon(address):
     try:
         lat_lon = resp_json_payload['results'][0]['geometry']['location']
     finally:
+        if lat_lon:
+            cashed_data[address] = lat_lon
+            cashAddresses(cashed_data)
         return lat_lon
-
 
 #l = get_lat_lon("Lviv")
 #print(l)
