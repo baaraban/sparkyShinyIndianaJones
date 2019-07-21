ea3d65daa459be37e2ec36240bf10115563bfff0
baaraban <perepichka@ucu.edu.ua>
2019-07-21 14:12:55 +0300

    Final things

--- utils/location_utils.py
+++ utils/location_utils.py
@@ -1,6 +1,9 @@
 import requests
 import pickle
-from config import GOOGLE_MAPS_API_KEY, GOOGLE_MAPS_API_URL, LOCATION_CASH_FILE
+
+GOOGLE_MAPS_API_KEY = 'AIzaSyCEE6oOLQ79t5IsEn9SeCE4mirqlFumcOY'
+GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
+LOCATION_CASH_FILE = "location_cash"
 
 def getCashedAddress(address):
     infile = open(LOCATION_CASH_FILE, 'rb')
